9621ab2f6ef5c96c4726e488e204bfbf1f7c5beb
Gerhard Halkjær <giga_34@hotmail.com>
2022-02-11 13:38:34 +0100

    sql database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,8 +6,55 @@ CREATE DATABASE devBlog;
 GO
 USE devBLog;
 GO
-CREATE TABLE Tag (Id int,Name nvarchar(50));
-CREATE TABLE TagCloud(TagId int, PostId int);
-CREATE TABLE Post (ID int, Title nvarchar(150),Content nvarchar(MAX),AutherId int);
-CREATE TABLE Links (Id int,PostId int,Link nvarchar(500));
-CREATE TABLE Files (Id int,PostId int, FileLocation nvarchar(300));
+CREATE TABLE Tag (
+Id int IDENTITY(1,1),
+Topic nvarchar(50),
+[Description] nvarchar(300),
+PRIMARY KEY (Id)
+);
+CREATE TABLE [User] (
+Id int IDENTITY(1,1),
+ForNavn nvarchar(50),
+EfterNavn nvarchar(50),
+PRIMARY KEY (Id)
+);
+CREATE TABLE Forfatter (
+Id int IDENTITY(1,1),
+UserId int not null,
+PostId int not null,
+PRIMARY KEY (Id),
+FOREIGN KEY (UserId) REFERENCES [User](Id)
+);
+CREATE TABLE Post (
+Id int IDENTITY(1,1),
+Title nvarchar(150),
+Content nvarchar(MAX),
+AutherId int,
+PRIMARY KEY (Id),
+FOREIGN KEY (AutherId) REFERENCES Forfatter(Id)
+);
+CREATE TABLE TagCloud (
+TagId int not null,
+PostId int not null
+PRIMARY KEY(TagId,PostId),
+FOREIGN KEY (TagId) REFERENCES Tag(Id),
+FOREIGN KEY (PostId) REFERENCES Post(Id)
+);
+CREATE TABLE Links (
+Id int IDENTITY(1,1),
+PostId int not null,
+Link nvarchar(500),
+PRIMARY KEY (Id),
+FOREIGN KEY (PostId) REFERENCES Post(Id)
+);
+CREATE TABLE Files (
+Id int IDENTITY(1,1),
+PostId int not null,
+FileLocation nvarchar(300),
+PRIMARY KEY (Id),
+FOREIGN KEY (PostId) REFERENCES Post(Id)
+);
+
+
+
+
--- devBlog/DAL/SQL/SqlDatabase.cs
+++ devBlog/DAL/SQL/SqlDatabase.cs
@@ -5,26 +5,28 @@ namespace devBlog.DAL.SQL;
 
 public class SqlDatabase : ISqlDatabase
 {
-    private readonly SqlConnection con;
+    private readonly string _strCon;
 
     public SqlDatabase()
     {
-        //TODO:move connection string to a sub folder for more security.
+        //move connection string to a sub folder for more security.
         IConfigurationRoot builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: false).Build();
-        string strCon = builder.GetConnectionString("devBlogDb");
-        con = new SqlConnection(strCon);
+        _strCon = builder.GetConnectionString("devBlogDb");
+        
     }
 
 
 
     public void CreatePost(Post post)
     {
+        SqlConnection con = new SqlConnection(_strCon);
         //TODO: CreatePost
         throw new NotImplementedException();
     }
 
     public List<Post> GetPosts()
     {
+        SqlConnection con = new SqlConnection(_strCon);
         //TODO: getPosts
         throw new NotImplementedException();
     }
